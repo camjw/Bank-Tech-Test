7c40bc62ef63489ece3ec0bd1dabfcc3c04f529c
Cameron Whitehead <camjw119@gmail.com>
2018-11-05 13:54:51 +0000

    Added helper functions to printer
    Added prettify transaction and prettify transactions to break up display statement function

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
+require_relative 'balance_calculator'
 
 # Class to print the bank statements to the console
 class Printer
   STATEMENT_HEADER = "date || credit || debit || balance\n"
+
+  def initialize(calculator: BalanceCalculator.new)
+    @calculator = calculator
+  end
+
   def display_statement(transactions)
-    parsed_transactions = parse_transactions(transactions)
+    print parse_transactions(transactions)
   end
 
   private
@@ -13,8 +19,21 @@ class Printer
     transactions.sort! { |first, second| second[0] <=> first[0] }
   end
 
+  def prettify_transaction(transaction)
+    middle_section = ['', transaction[1]]
+    middle_section.reverse! if transaction[2] == 'credit'
+    return (transaction[0] + middle_section + transaction[3]).join(' ||')
+  end
+
+  def prettify_all_transactions(transactions)
+    transactions.each do |transaction|
+      transaction = prettify_transaction(transaction)
+    end
+  end
+
   def parse_transactions(transactions)
     sort_by_date(transactions)
-    calculate_balance(transactions)
+    @calculator.append_balances(transactions)
+    prettify_all_transactions(transactions)
   end
 end
