bd78d0995a9f8fe3d4c67a8cfbe9d5d0c289dfe9
Cameron Whitehead <camjw119@gmail.com>
2018-11-05 13:30:55 +0000

    Added balance calculator class
    BalanceCalculator can append the balances to a list of transactions. Currently does not handle the case when transactions are not in data order

---
+++ lib/balance_calculator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Class calculates and appends the balances to a list of transactions
+class BalanceCalculator
+  def append_balances(transactions)
+    balance = 0
+    transactions.reverse_each do |tran|
+      tran[2] == 'deposit' ? balance += tran[1] : balance -= tran[1]
+      tran << balance
+    end
+    transactions
+  end
+end
--- spec/balance_calculator_spec.rb
+++ spec/balance_calculator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'balance_calculator'
 
 RSpec.describe BalanceCalculator do
