ededd70f69a3253fda50d74a379c88b29966af69
Cameron Whitehead <camjw119@gmail.com>
2018-11-05 14:19:28 +0000

    Interface tests passing
    Added two-line display_statement method to interface to pass tests
    
    Also renamed mock_transaction to mock_history for better readability

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,20 +1,28 @@
 # frozen_string_literal: true
 
 require_relative 'transaction_history'
+require_relative 'printer'
+
 
 # Interface class to interact with user
 class Interface
   INVALID_TYPE_ERROR = "This method can only take 'deposit' and 'withdrawal'"\
     ' as third parameters'
 
-  def initialize(history: TransactionHistory.new)
+  def initialize(history: TransactionHistory.new, printer: Printer.new)
     @history = history
+    @printer = printer
   end
 
   def transaction(date, amount, type)
     check_for_valid_type(type)
     @history.store_transaction([date, amount, type])
   end
+
+  def display_statement
+    transactions = @history.transactions
+    @printer.display_statement(transactions)
+  end 
 
   private
 
--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -3,13 +3,13 @@
 require 'interface'
 
 RSpec.describe Interface do
-  let(:mock_transaction) do double :mock_transaction, store_transaction: nil,
+  let(:mock_history) do double :mock_history, store_transaction: nil,
     transactions: ['01/01/1994', 100.0, 'deposit']
   end
   let(:mock_printer) { double :mock_printer, display_statement: nil }
 
   subject do
-    described_class.new(history: mock_transaction, printer: mock_printer)
+    described_class.new(history: mock_history, printer: mock_printer)
   end
 
   describe '#transaction' do
@@ -17,14 +17,14 @@ RSpec.describe Interface do
       it 'instructs the TransactionHistory to store a deposit' do
         subject.transaction('01/01/1994', 100.0, 'deposit')
         data = ['01/01/1994', 100.0, 'deposit']
-        expect(mock_transaction).to have_received(:store_transaction).with(data)
+        expect(mock_history).to have_received(:store_transaction).with(data)
       end
     end
     context 'when called with withdrawal' do
       it 'instructs the TransactionHistory to store a withdrawal' do
         subject.transaction('01/01/1994', 100.0, 'withdrawal')
         data = ['01/01/1994', 100.0, 'withdrawal']
-        expect(mock_transaction).to have_received(:store_transaction).with(data)
+        expect(mock_history).to have_received(:store_transaction).with(data)
       end
     end
     context 'when called with a different string' do
@@ -40,12 +40,12 @@ RSpec.describe Interface do
   describe '#display_statement' do
     it 'asks the TransactionHistory for the previous transactions' do
       subject.display_statement
-      expect(:mock_printer).to have_received(:transactions)
+      expect(mock_history).to have_received(:transactions)
     end
     it 'instructs the printer to display a statement' do
       subject.display_statement
       data = ['01/01/1994', 100.0, 'deposit']
-      expect(:mock_printer).to have_received(:display_statement).with(data)
+      expect(mock_printer).to have_received(:display_statement).with(data)
     end
   end
 end
